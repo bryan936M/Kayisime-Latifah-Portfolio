04a24fc141147ee1b2e72affc03fcc9ce25054c6
Bryan <bryanmurasira@gmail.co>
2023-08-12 17:48:35 +0200

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,10 @@ function App() {
   return (
     <>
       <div className="portfolio__header">
-        <a href="Resume.pdf" download='My Resume.pdf' >Resume</a>
+        <div className="portfolio__links">
+          <a href="Resume.pdf">Resume</a>
+          <a href="cover-letter.pdf">Cover Letter</a>
+        </div>
         <button>Hire Me</button>
       </div>
       <section className="main">
@@ -17,12 +20,13 @@ function App() {
             Hi, I'm Latifah <br /> Kayisime.
           </h1>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt,
-            a. Voluptate tempora rem molestias deleniti fugiat rerum dolore
-            quod. Laudantium, optio?
+            Throughout my work experience, I have demonstrated high motivation,
+            work ethic, and teamwork to achieve my goals. At 22, I am proud of
+            my achievements and embrace opportunities that broaden my learning
+            perspective to better myself.
           </p>
           <div className="social-media">
-            <a href="#">
+            <a href="https://www.linkedin.com/in/latifah-kayisime-58b30026b/">
               <img className="logo linkedin" src={linkedin} alt="" />
             </a>
             <a href="#">
@@ -47,22 +51,27 @@ function App() {
           <img className="latifah-mini" src={latifah} alt="face" />
           <div className="about-me-text">
             <p className="primary">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatibus, incidunt adipisci! Autem vitae hic iusto rem
-              temporibus alias ipsa natus possimus, placeat laborum totam minus
-              commodi optio, officiis eum recusandae consectetur! Voluptatum,
-              aut. Sit nobis dolores, repellendus natus iure a sequi unde
-              laborum corporis quasi beatae excepturi temporibus libero minima?
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem
-              maiores officia sit cum iure cupiditate nihil, repellendus,
-              nostrum dignissimos ipsa fugiat, explicabo voluptatum officiis
-              neque enim quis.
+              Passionate, driven entrepreneur with a deep-rooted love for
+              planetary health. From a strong academic foundation in
+              Mathematics, Physics, and Geography to a proactive role as a
+              customer care representative at Connexteck, bridging language
+              barriers for Canadian clients. Eagerly awaiting the Giants of
+              Africa Festival Youth Internship, having previously assisted the
+              Africa Soft Power Summit podcast. Prior experience includes
+              collaborating with Stanford University on vital data collection
+              and enriching sales acumen in a Handcraft Shop. A history
+              enthusiast who, in 2018, spearheaded a team at R.A.L.C. to curate
+              Rwanda's historical gems into a new library.
             </p>
 
             <p className="secondary">
-              My Skills include: Lorem, ipsum-dolor, sit-amet,
-              consectetur,adipisicing-elit. Numquam-eaque, earum,
-              beatae-sapiente, neque- doloremque-quos, quidem-autem-iste, modi!
+              <strong>My Skills include:</strong> Customer Service - Language
+              Proficiency - Interpersonal Communication - Project Management -
+              Cultural Awareness - Data Collection - Sales and Marketing -
+              Research and Analysis - Adaptability - Academic Excellence -
+              Leadership - Problem Solving. These skills collectively contribute
+              to the individual's well-rounded profile and ability to excel in
+              various roles and contexts.
             </p>
           </div>
         </div>
@@ -74,31 +83,35 @@ function App() {
           <div className="talent one">
             <img src={latifah} alt="talent picture" />
             <br />
-            <span className="talent-name">Lorem, ipsum dolor.</span>
+            <span className="talent-name">Multilingual Proficiency</span>
             <br />
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Excepturi provident quaerat, assumenda velit quod soluta.
+              Ability to communicate fluently in both English and French is a
+              valuable talent, enabling effective interaction with a diverse
+              range of clients and collaborators.
             </p>
           </div>
           <div className="talent two">
             <img src={latifah} alt="talent picture" />
             <br />
-            <span className="talent-name">Lorem, ipsum dolor.</span>
+            <span className="talent-name">Project Management</span>
             <br />
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Excepturi provident quaerat, assumenda velit quod soluta.
+              Experienced in leading a team of interns and organizing historical
+              documents for a library demonstrates strong leadership and project
+              management skills.
             </p>
           </div>
           <div className="talent three">
             <img src={latifah} alt="talent picture" />
             <br />
-            <span className="talent-name">Lorem, ipsum dolor.</span>
+            <span className="talent-name">Customer Engagement and Sales</span>
             <br />
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Excepturi provident quaerat, assumenda velit quod soluta.
+              Performance as a customer care representative and salesperson,
+              along with their capacity to present, promote, and sell products
+              and services, highlights a talent for engaging with customers,
+              understanding their needs, and driving successful sales outcomes.
             </p>
           </div>
         </div>
@@ -109,32 +122,22 @@ function App() {
         <div className="experiences">
           <div className="experience one">
             <img src={latifah} alt="Work Picture" />
-            <span>Lorem Ipsum</span>
+            <span>Africa Soft power Summit</span>
             <a href="#">View Company</a>
           </div>
           <div className="experience one">
             <img src={latifah} alt="Work Picture" />
-            <span>Lorem Ipsum</span>
+            <span>Stanford data collection project</span>
             <a href="#">View Company</a>
           </div>
           <div className="experience one">
             <img src={latifah} alt="Work Picture" />
-            <span>Lorem Ipsum</span>
+            <span>Handcraft Shop as a Salesperson</span>
             <a href="#">View Company</a>
           </div>
           <div className="experience one">
             <img src={latifah} alt="Work Picture" />
-            <span>Lorem Ipsum</span>
-            <a href="#">View Company</a>
-          </div>
-          <div className="experience one">
-            <img src={latifah} alt="Work Picture" />
-            <span>Lorem Ipsum</span>
-            <a href="#">View Company</a>
-          </div>
-          <div className="experience one">
-            <img src={latifah} alt="Work Picture" />
-            <span>Lorem Ipsum</span>
+            <span>Rwanda Academy of Language and Culture</span>
             <a href="#">View Company</a>
           </div>
         </div>
@@ -176,7 +179,6 @@ function App() {
             </div>
           </div>
         </div>
-        
       </section>
     </>
   );
